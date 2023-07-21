6400babd933c3982bca335b6ead51eaefa2e2fbe
younhoso <thdbsgh3443@kakao.com>
2023-07-21 14:53:47 +0900

    update: 비동기 요청을 통한 데이터 연동

--- mini-cart-main/src/index.js
+++ mini-cart-main/src/index.js
@@ -1,1 +1,9 @@
+import {api} from "./api/getProductData.js";
 // 이 곳에 정답 코드를 작성해주세요.
+
+const feachProductData = async () => {
+  const result = await api.getProductData();
+  console.log(result)
+};
+
+feachProductData();
